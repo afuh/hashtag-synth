b73477411f9c4f71c63bc95dac55910fc00c8ad0
Axel <axelfuh@gmail.com>
2018-07-01 21:44:55 +0200

    Add a hashtags file

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  hashtags: ["#refugees", "#refugee", "#RefugeeWeek2018", "#RefugeeWeek", "#RefugeeDay", "#Flüchtlinge", "#migrants", "#fuckAFD", "#noafd", "#equality", "#AsylumSeekers", "#refugiado", "#refugiados", "#Rèfugiés", "#peace", "#paz", "#spreadlove", "#stophate"]
+}
