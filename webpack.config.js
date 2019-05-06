32779a1f5aa2937084cec6833582d8d4556e8213
Axel <axelfuh@gmail.com>
2019-05-06 15:36:35 +0200

    Update name

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Twitter audio',
+      title: 'Hashtag synth',
       minify: {
         collapseWhitespace: true
       },
